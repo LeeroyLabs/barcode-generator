cc09e79782e0f3c28cba3c2db1a60e26bf9c0d9f
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2023-02-15 11:48:24 -0500

    added text support to SVG format

--- src/BarcodeGeneratorSVG.php
+++ src/BarcodeGeneratorSVG.php
@@ -9,24 +9,30 @@ class BarcodeGeneratorSVG extends BarcodeGenerator
      *
      * @param $barcode (string) code to print
      * @param $type (const) type of barcode
-     * @param $widthFactor (int) Minimum width of a single bar in user units.
+     * @param $widthFactor (float) Minimum width of a single bar in user units.
      * @param $height (int) Height of barcode in user units.
      * @param $foregroundColor (string) Foreground color (in SVG format) for bar elements (background is transparent).
+     * @param  bool  $showValue (bool) Show the value of the code under the barcode
      * @return string SVG code.
      * @public
      */
-    public function getBarcode($barcode, $type, int $widthFactor = 2, int $height = 30, string $foregroundColor = 'black')
+    public function getBarcode($barcode, $type, float $widthFactor = 2, int $height = 30, string $foregroundColor = 'black', bool $showValue = false)
     {
         $barcodeData = $this->getBarcodeData($barcode, $type);
 
         // replace table for special characters
         $repstr = ["\0" => '', '&' => '&amp;', '<' => '&lt;', '>' => '&gt;'];
 
-        $width = round(($barcodeData->getWidth() * $widthFactor), 3);
+        // Allow to have values between to full ints (ex: 3.5) then whatever value is given precision should be 0
+        $width = round(($barcodeData->getWidth() * $widthFactor), 0);
+
+        if ($showValue) {
+            $SVGheight = $height + 48;
+        }
 
         $svg = '<?xml version="1.0" standalone="no" ?>' . PHP_EOL;
         $svg .= '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' . PHP_EOL;
-        $svg .= '<svg width="' . $width . '" height="' . $height . '" viewBox="0 0 ' . $width . ' ' . $height . '" version="1.1" xmlns="http://www.w3.org/2000/svg">' . PHP_EOL;
+        $svg .= '<svg width="' . $width . '" height="' . $SVGheight . '" viewBox="0 0 ' . $width . ' ' . $SVGheight . '" version="1.1" xmlns="http://www.w3.org/2000/svg">' . PHP_EOL;
         $svg .= "\t" . '<desc>' . strtr($barcodeData->getBarcode(), $repstr) . '</desc>' . PHP_EOL;
         $svg .= "\t" . '<g id="bars" fill="' . $foregroundColor . '" stroke="none">' . PHP_EOL;
 
@@ -45,6 +51,13 @@ class BarcodeGeneratorSVG extends BarcodeGenerator
 
             $positionHorizontal += $barWidth;
         }
+
+        if ($showValue) {
+            $y = $SVGheight - 24;
+            $x = ceil($width / 2);
+            $svg .= "\t\t<text style=\"font: 20px monospace\" text-anchor=\"middle\" x=\"$x\" y=\"$y\">$barcode</text>\n";
+        }
+
         $svg .= "\t</g>" . PHP_EOL;
         $svg .= '</svg>' . PHP_EOL;
 
--- tests/BarcodeSvgTest.php
+++ tests/BarcodeSvgTest.php
@@ -7,8 +7,7 @@ class BarcodeSvgTest extends TestCase
     public function test_svg_barcode_generator_can_generate_ean_13_barcode()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
-        $generated = $generator->getBarcode('081231723897', $generator::TYPE_EAN_13);
-
-        $this->assertStringEqualsFile('tests/verified-files/081231723897-ean13.svg', $generated);
+        $generated = $generator->getBarcode('ASKIPP23014462339', $generator::TYPE_CODE_128, 2.1, 110, 'black', true);
+        $this->assertStringContainsString('ASKIPP23014462339', $generated);
     }
 }
